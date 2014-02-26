3d09946305b16e46d01cf4a96c577b348cef1163
Josh Keegan <josh.keegan@gmx.com>
2014-02-26 22:51:56 +0000

    Load the Bitmaps into a queue to be processed

--- DataMarkup/MainForm.Designer.cs
+++ DataMarkup/MainForm.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picBoxWordsearchImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxWordsearchImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxWordsearchImage
+            // 
+            this.picBoxWordsearchImage.Location = new System.Drawing.Point(13, 13);
+            this.picBoxWordsearchImage.Name = "picBoxWordsearchImage";
+            this.picBoxWordsearchImage.Size = new System.Drawing.Size(1465, 840);
+            this.picBoxWordsearchImage.TabIndex = 0;
+            this.picBoxWordsearchImage.TabStop = false;
+            this.picBoxWordsearchImage.Click += new System.EventHandler(this.picBoxWordsearchImage_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1503, 877);
+            this.Controls.Add(this.picBoxWordsearchImage);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxWordsearchImage)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picBoxWordsearchImage;
     }
 }
 
--- DataMarkup/MainForm.cs
+++ DataMarkup/MainForm.cs
@@ -6,6 +6,7 @@
  */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,13 +16,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using ImageMarkup;
+using SharedHelpers;
+
 namespace DataMarkup
 {
     public partial class MainForm : Form
     {
+        //Constants
+        private const string RAW_IMAGE_DIR = "images";
+
+        //Private vars
+        private Queue<Bitmap> toProcess;
+
         public MainForm()
         {
             InitializeComponent();
+
+            //Load in the meta data for images that are already processed
+            ImageMarkupDatabase.LoadDatabase();
+
+            //Load in the raw images to process
+            List<Bitmap> bitmaps = ImageLoader.LoadAllImagesInSubdirs(RAW_IMAGE_DIR);
+            toProcess = new Queue<Bitmap>(bitmaps);
+        }
+
+        private void picBoxWordsearchImage_Click(object sender, EventArgs e)
+        {
+            //TODO: Get the co-ords of the click & store them on this picture as wordsearch corner co-ords
         }
     }
 }
