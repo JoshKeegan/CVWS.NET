8a28b3382ef771a2d442736c00017d4fb81faddf
Josh Keegan <josh.keegan@gmx.com>
2014-02-26 16:26:01 +0000

    Initial Commit

---
+++ DataMarkup/MainForm.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Data Markup
+ * Main Form Class - Main GUI Window
+ * By Josh Keegan 26/02/2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataMarkup
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
