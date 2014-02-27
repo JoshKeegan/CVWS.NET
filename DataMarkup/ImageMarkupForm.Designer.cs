7cdaa2554201e060e5bcc8bdae56f4f934990672
Josh Keegan <josh.keegan@gmx.com>
2014-02-27 15:26:14 +0000

    Allow for a wordsearch images coordinates to be entered by clicking it's corners on the image. Then draw a grid onto the image (account for perspective) given the number of rows & cols

--- DataMarkup/ImageMarkupForm.Designer.cs
+++ DataMarkup/ImageMarkupForm.Designer.cs
@@ -32,6 +32,26 @@
             this.lblToPorcessLength = new System.Windows.Forms.Label();
             this.btnNextImage = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAddWordsearchImage = new System.Windows.Forms.Button();
+            this.lblTopLeft = new System.Windows.Forms.Label();
+            this.lblCoordinates = new System.Windows.Forms.Label();
+            this.txtTopLeftX = new System.Windows.Forms.TextBox();
+            this.txtTopLeftY = new System.Windows.Forms.TextBox();
+            this.txtTopRightY = new System.Windows.Forms.TextBox();
+            this.txtTopRightX = new System.Windows.Forms.TextBox();
+            this.lblTopRight = new System.Windows.Forms.Label();
+            this.txtBottomRightY = new System.Windows.Forms.TextBox();
+            this.txtBottomRightX = new System.Windows.Forms.TextBox();
+            this.lblBottomRight = new System.Windows.Forms.Label();
+            this.txtBottomLeftY = new System.Windows.Forms.TextBox();
+            this.txtBottomLeftX = new System.Windows.Forms.TextBox();
+            this.lblBottomLeft = new System.Windows.Forms.Label();
+            this.lblNumRows = new System.Windows.Forms.Label();
+            this.lblNumCols = new System.Windows.Forms.Label();
+            this.txtNumRows = new System.Windows.Forms.TextBox();
+            this.txtNumCols = new System.Windows.Forms.TextBox();
+            this.lblWordsearchId = new System.Windows.Forms.Label();
+            this.txtWordsearchId = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxWordsearchImage)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -40,7 +60,7 @@
             // 
             this.picBoxWordsearchImage.Location = new System.Drawing.Point(3, 3);
             this.picBoxWordsearchImage.Name = "picBoxWordsearchImage";
-            this.picBoxWordsearchImage.Size = new System.Drawing.Size(1783, 927);
+            this.picBoxWordsearchImage.Size = new System.Drawing.Size(1483, 927);
             this.picBoxWordsearchImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.picBoxWordsearchImage.TabIndex = 0;
             this.picBoxWordsearchImage.TabStop = false;
@@ -49,7 +69,7 @@
             // lblToPorcessLength
             // 
             this.lblToPorcessLength.AutoSize = true;
-            this.lblToPorcessLength.Location = new System.Drawing.Point(1807, 8);
+            this.lblToPorcessLength.Location = new System.Drawing.Point(1504, 6);
             this.lblToPorcessLength.Name = "lblToPorcessLength";
             this.lblToPorcessLength.Size = new System.Drawing.Size(64, 13);
             this.lblToPorcessLength.TabIndex = 1;
@@ -58,7 +78,7 @@
             // btnNextImage
             // 
             this.btnNextImage.Image = global::DataEntryGUI.Properties.Resources.right;
-            this.btnNextImage.Location = new System.Drawing.Point(1810, 25);
+            this.btnNextImage.Location = new System.Drawing.Point(1507, 23);
             this.btnNextImage.Name = "btnNextImage";
             this.btnNextImage.Size = new System.Drawing.Size(82, 71);
             this.btnNextImage.TabIndex = 2;
@@ -72,18 +92,199 @@
             this.panel1.Controls.Add(this.picBoxWordsearchImage);
             this.panel1.Location = new System.Drawing.Point(12, 9);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1789, 933);
+            this.panel1.Size = new System.Drawing.Size(1489, 933);
             this.panel1.TabIndex = 3;
             // 
-            // ImageMarkupGUI
+            // btnAddWordsearchImage
+            // 
+            this.btnAddWordsearchImage.Location = new System.Drawing.Point(1507, 783);
+            this.btnAddWordsearchImage.Name = "btnAddWordsearchImage";
+            this.btnAddWordsearchImage.Size = new System.Drawing.Size(75, 48);
+            this.btnAddWordsearchImage.TabIndex = 4;
+            this.btnAddWordsearchImage.Text = "Add Word search Image";
+            this.btnAddWordsearchImage.UseVisualStyleBackColor = true;
+            this.btnAddWordsearchImage.Click += new System.EventHandler(this.btnAddWordsearchImage_Click);
+            // 
+            // lblTopLeft
+            // 
+            this.lblTopLeft.AutoSize = true;
+            this.lblTopLeft.Location = new System.Drawing.Point(1507, 495);
+            this.lblTopLeft.Name = "lblTopLeft";
+            this.lblTopLeft.Size = new System.Drawing.Size(50, 13);
+            this.lblTopLeft.TabIndex = 6;
+            this.lblTopLeft.Text = "Top Left:";
+            // 
+            // lblCoordinates
+            // 
+            this.lblCoordinates.AutoSize = true;
+            this.lblCoordinates.Location = new System.Drawing.Point(1507, 473);
+            this.lblCoordinates.Name = "lblCoordinates";
+            this.lblCoordinates.Size = new System.Drawing.Size(66, 13);
+            this.lblCoordinates.TabIndex = 7;
+            this.lblCoordinates.Text = "Coordinates:";
+            // 
+            // txtTopLeftX
+            // 
+            this.txtTopLeftX.Location = new System.Drawing.Point(1580, 492);
+            this.txtTopLeftX.Name = "txtTopLeftX";
+            this.txtTopLeftX.Size = new System.Drawing.Size(32, 20);
+            this.txtTopLeftX.TabIndex = 8;
+            // 
+            // txtTopLeftY
+            // 
+            this.txtTopLeftY.Location = new System.Drawing.Point(1618, 492);
+            this.txtTopLeftY.Name = "txtTopLeftY";
+            this.txtTopLeftY.Size = new System.Drawing.Size(32, 20);
+            this.txtTopLeftY.TabIndex = 9;
+            // 
+            // txtTopRightY
+            // 
+            this.txtTopRightY.Location = new System.Drawing.Point(1618, 518);
+            this.txtTopRightY.Name = "txtTopRightY";
+            this.txtTopRightY.Size = new System.Drawing.Size(32, 20);
+            this.txtTopRightY.TabIndex = 12;
+            // 
+            // txtTopRightX
+            // 
+            this.txtTopRightX.Location = new System.Drawing.Point(1580, 518);
+            this.txtTopRightX.Name = "txtTopRightX";
+            this.txtTopRightX.Size = new System.Drawing.Size(32, 20);
+            this.txtTopRightX.TabIndex = 11;
+            // 
+            // lblTopRight
+            // 
+            this.lblTopRight.AutoSize = true;
+            this.lblTopRight.Location = new System.Drawing.Point(1507, 521);
+            this.lblTopRight.Name = "lblTopRight";
+            this.lblTopRight.Size = new System.Drawing.Size(57, 13);
+            this.lblTopRight.TabIndex = 10;
+            this.lblTopRight.Text = "Top Right:";
+            // 
+            // txtBottomRightY
+            // 
+            this.txtBottomRightY.Location = new System.Drawing.Point(1618, 544);
+            this.txtBottomRightY.Name = "txtBottomRightY";
+            this.txtBottomRightY.Size = new System.Drawing.Size(32, 20);
+            this.txtBottomRightY.TabIndex = 15;
+            // 
+            // txtBottomRightX
+            // 
+            this.txtBottomRightX.Location = new System.Drawing.Point(1580, 544);
+            this.txtBottomRightX.Name = "txtBottomRightX";
+            this.txtBottomRightX.Size = new System.Drawing.Size(32, 20);
+            this.txtBottomRightX.TabIndex = 14;
+            // 
+            // lblBottomRight
+            // 
+            this.lblBottomRight.AutoSize = true;
+            this.lblBottomRight.Location = new System.Drawing.Point(1507, 547);
+            this.lblBottomRight.Name = "lblBottomRight";
+            this.lblBottomRight.Size = new System.Drawing.Size(71, 13);
+            this.lblBottomRight.TabIndex = 13;
+            this.lblBottomRight.Text = "Bottom Right:";
+            // 
+            // txtBottomLeftY
+            // 
+            this.txtBottomLeftY.Location = new System.Drawing.Point(1618, 570);
+            this.txtBottomLeftY.Name = "txtBottomLeftY";
+            this.txtBottomLeftY.Size = new System.Drawing.Size(32, 20);
+            this.txtBottomLeftY.TabIndex = 18;
+            // 
+            // txtBottomLeftX
+            // 
+            this.txtBottomLeftX.Location = new System.Drawing.Point(1580, 570);
+            this.txtBottomLeftX.Name = "txtBottomLeftX";
+            this.txtBottomLeftX.Size = new System.Drawing.Size(32, 20);
+            this.txtBottomLeftX.TabIndex = 17;
+            // 
+            // lblBottomLeft
+            // 
+            this.lblBottomLeft.AutoSize = true;
+            this.lblBottomLeft.Location = new System.Drawing.Point(1507, 573);
+            this.lblBottomLeft.Name = "lblBottomLeft";
+            this.lblBottomLeft.Size = new System.Drawing.Size(64, 13);
+            this.lblBottomLeft.TabIndex = 16;
+            this.lblBottomLeft.Text = "Bottom Left:";
+            // 
+            // lblNumRows
+            // 
+            this.lblNumRows.AutoSize = true;
+            this.lblNumRows.Location = new System.Drawing.Point(1513, 615);
+            this.lblNumRows.Name = "lblNumRows";
+            this.lblNumRows.Size = new System.Drawing.Size(37, 13);
+            this.lblNumRows.TabIndex = 19;
+            this.lblNumRows.Text = "Rows:";
+            // 
+            // lblNumCols
+            // 
+            this.lblNumCols.AutoSize = true;
+            this.lblNumCols.Location = new System.Drawing.Point(1513, 641);
+            this.lblNumCols.Name = "lblNumCols";
+            this.lblNumCols.Size = new System.Drawing.Size(50, 13);
+            this.lblNumCols.TabIndex = 20;
+            this.lblNumCols.Text = "Columns:";
+            // 
+            // txtNumRows
+            // 
+            this.txtNumRows.Location = new System.Drawing.Point(1580, 612);
+            this.txtNumRows.Name = "txtNumRows";
+            this.txtNumRows.Size = new System.Drawing.Size(32, 20);
+            this.txtNumRows.TabIndex = 21;
+            this.txtNumRows.TextChanged += new System.EventHandler(this.txtNumRows_TextChanged);
+            // 
+            // txtNumCols
+            // 
+            this.txtNumCols.Location = new System.Drawing.Point(1580, 638);
+            this.txtNumCols.Name = "txtNumCols";
+            this.txtNumCols.Size = new System.Drawing.Size(32, 20);
+            this.txtNumCols.TabIndex = 22;
+            this.txtNumCols.TextChanged += new System.EventHandler(this.txtNumCols_TextChanged);
+            // 
+            // lblWordsearchId
+            // 
+            this.lblWordsearchId.AutoSize = true;
+            this.lblWordsearchId.Location = new System.Drawing.Point(1527, 693);
+            this.lblWordsearchId.Name = "lblWordsearchId";
+            this.lblWordsearchId.Size = new System.Drawing.Size(85, 13);
+            this.lblWordsearchId.TabIndex = 23;
+            this.lblWordsearchId.Text = "Word search ID:";
+            // 
+            // txtWordsearchId
+            // 
+            this.txtWordsearchId.Location = new System.Drawing.Point(1618, 690);
+            this.txtWordsearchId.Name = "txtWordsearchId";
+            this.txtWordsearchId.Size = new System.Drawing.Size(100, 20);
+            this.txtWordsearchId.TabIndex = 24;
+            // 
+            // ImageMarkupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1904, 952);
+            this.Controls.Add(this.txtWordsearchId);
+            this.Controls.Add(this.lblWordsearchId);
+            this.Controls.Add(this.txtNumCols);
+            this.Controls.Add(this.txtNumRows);
+            this.Controls.Add(this.lblNumCols);
+            this.Controls.Add(this.lblNumRows);
+            this.Controls.Add(this.txtBottomLeftY);
+            this.Controls.Add(this.txtBottomLeftX);
+            this.Controls.Add(this.lblBottomLeft);
+            this.Controls.Add(this.txtBottomRightY);
+            this.Controls.Add(this.txtBottomRightX);
+            this.Controls.Add(this.lblBottomRight);
+            this.Controls.Add(this.txtTopRightY);
+            this.Controls.Add(this.txtTopRightX);
+            this.Controls.Add(this.lblTopRight);
+            this.Controls.Add(this.txtTopLeftY);
+            this.Controls.Add(this.txtTopLeftX);
+            this.Controls.Add(this.lblCoordinates);
+            this.Controls.Add(this.lblTopLeft);
+            this.Controls.Add(this.btnAddWordsearchImage);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnNextImage);
             this.Controls.Add(this.lblToPorcessLength);
-            this.Name = "ImageMarkupGUI";
+            this.Name = "ImageMarkupForm";
             this.Text = "Image Markup";
             ((System.ComponentModel.ISupportInitialize)(this.picBoxWordsearchImage)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -99,6 +300,26 @@
         private System.Windows.Forms.Label lblToPorcessLength;
         private System.Windows.Forms.Button btnNextImage;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAddWordsearchImage;
+        private System.Windows.Forms.Label lblTopLeft;
+        private System.Windows.Forms.Label lblCoordinates;
+        private System.Windows.Forms.TextBox txtTopLeftX;
+        private System.Windows.Forms.TextBox txtTopLeftY;
+        private System.Windows.Forms.TextBox txtTopRightY;
+        private System.Windows.Forms.TextBox txtTopRightX;
+        private System.Windows.Forms.Label lblTopRight;
+        private System.Windows.Forms.TextBox txtBottomRightY;
+        private System.Windows.Forms.TextBox txtBottomRightX;
+        private System.Windows.Forms.Label lblBottomRight;
+        private System.Windows.Forms.TextBox txtBottomLeftY;
+        private System.Windows.Forms.TextBox txtBottomLeftX;
+        private System.Windows.Forms.Label lblBottomLeft;
+        private System.Windows.Forms.Label lblNumRows;
+        private System.Windows.Forms.Label lblNumCols;
+        private System.Windows.Forms.TextBox txtNumRows;
+        private System.Windows.Forms.TextBox txtNumCols;
+        private System.Windows.Forms.Label lblWordsearchId;
+        private System.Windows.Forms.TextBox txtWordsearchId;
     }
 }
 
--- DataMarkup/ImageMarkupForm.cs
+++ DataMarkup/ImageMarkupForm.cs
@@ -12,10 +12,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using AForge;
+using AForge.Imaging;
 
 using ImageMarkup;
 using SharedHelpers;
@@ -31,6 +35,7 @@ namespace DataEntryGUI
         //Private vars
         private Queue<KeyValuePair<Bitmap, string>> toProcess; //Store the hash along with the Bitmap so we don't have to recompute it later
         private KeyValuePair<Bitmap, string> currentBitmap;
+        List<WordsearchImage> wordsearchImages; // For the images of word searches in the current image
 
         public ImageMarkupForm()
         {
@@ -62,20 +67,73 @@ namespace DataEntryGUI
 
         private void picBoxWordsearchImage_Click(object sender, EventArgs e)
         {
-            //TODO: Get the co-ords of the click & store them on this picture as wordsearch corner co-ords
+            //Get the co-ords of the click
+            MouseEventArgs mousePress = e as MouseEventArgs;
+            int x = mousePress.X;
+            int y = mousePress.Y;
+
+            //Put the coordinates in the relevant box
+            if(txtTopLeftX.Text == "" && txtTopLeftY.Text == "")
+            {
+                txtTopLeftX.Text = x.ToString();
+                txtTopLeftY.Text = y.ToString();
+            }
+            else if(txtTopRightX.Text == "" && txtTopRightY.Text == "")
+            {
+                txtTopRightX.Text = x.ToString();
+                txtTopRightY.Text = y.ToString();
+            }
+            else if(txtBottomRightX.Text == "" && txtBottomRightY.Text == "")
+            {
+                txtBottomRightX.Text = x.ToString();
+                txtBottomRightY.Text = y.ToString();
+            }
+            else if(txtBottomLeftX.Text == "" && txtBottomLeftY.Text == "")
+            {
+                txtBottomLeftX.Text = x.ToString();
+                txtBottomLeftY.Text = y.ToString();
+            }
+            else
+            {
+                MessageBox.Show("All points have been entered for this Wordsearch Image");
+            }
         }
 
         private void btnNextImage_Click(object sender, EventArgs e)
         {
-            //TODO: Save the markup on the current image
+            //TODO: Save the markup on the current image (if we have just processed one)
 
-            //Get the next image
-            currentBitmap = toProcess.Dequeue();
+            //If there is another image to get
+            if(toProcess.Count > 0)
+            {
+                //Get the next image
+                currentBitmap = toProcess.Dequeue();
 
-            //Resize the picture box to the 
-            picBoxWordsearchImage.Image = currentBitmap.Key;
+                //Resize the picture box to the 
+                picBoxWordsearchImage.Image = currentBitmap.Key;
 
-            updateLblToProcessLength();
+                updateLblToProcessLength();
+            }
+            else //Otherwise there are no more images to be processed
+            {
+                picBoxWordsearchImage.Image = new Bitmap(1, 1);
+            }
+            
+        }
+
+        private void txtNumRows_TextChanged(object sender, EventArgs e)
+        {
+            drawRowsAndColsOnCurrentWordsearch();
+        }
+
+        private void txtNumCols_TextChanged(object sender, EventArgs e)
+        {
+            drawRowsAndColsOnCurrentWordsearch();
+        }
+
+        private void btnAddWordsearchImage_Click(object sender, EventArgs e)
+        {
+
         }
 
         /*
@@ -84,6 +142,107 @@ namespace DataEntryGUI
         private void updateLblToProcessLength()
         {
             lblToPorcessLength.Text = toProcess.Count.ToString();
+        }
+
+        private void drawRowsAndColsOnCurrentWordsearch()
+        {
+            //Get Rows & Cols
+            uint rows, cols;
+            uint.TryParse(txtNumRows.Text, out rows);
+            uint.TryParse(txtNumCols.Text, out cols);
+
+            //Check we have all four coordinates of the wordsearch image
+            if(txtTopLeftX.Text != "" && txtTopLeftY.Text != "" &&
+                txtTopRightX.Text != "" && txtTopRightY.Text != "" &&
+                txtBottomRightX.Text != "" && txtBottomRightY.Text != "" &&
+                txtBottomLeftX.Text != "" && txtBottomLeftY.Text != "")
+            {
+                Bitmap drawnOn = drawRowsAndColsOnImage(currentBitmap.Key, 
+                    getTopLeftCoordinate(), getTopRightCoordinate(), 
+                    getBottomRightCoordinate(), getBottomLeftCoordinate(), 
+                    rows, cols);
+                picBoxWordsearchImage.Image = drawnOn;
+            }
+        }
+
+        private Bitmap drawRowsAndColsOnImage(Bitmap img, IntPoint topLeft, 
+            IntPoint topRight, IntPoint bottomRight, IntPoint bottomLeft, 
+            uint rows, uint cols)
+        {
+            return drawRowsAndColsOnImage(img, topLeft, topRight, bottomRight, 
+                bottomLeft, rows, cols, Color.Red);
+        }
+
+        private Bitmap drawRowsAndColsOnImage(Bitmap imgOrig, IntPoint topLeft, 
+            IntPoint topRight, IntPoint bottomRight, IntPoint bottomLeft, 
+            uint rows, uint cols, Color colour)
+        {
+            Bitmap img = new Bitmap(imgOrig);
+
+            //Lock image for read write so we can alter it
+            BitmapData imgData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height),
+                ImageLockMode.ReadWrite, img.PixelFormat);
+
+            //Draw Lines at regular intervals for rows
+            Drawing.Line(imgData, topLeft, topRight, colour);
+            for(int i = 1; i <= rows; i++)
+            {
+                int leftSideRowBottomX = (int)(((bottomLeft.X - topLeft.X) / (double)rows) * i + topLeft.X);
+                int leftSideRowBottomY = (int)(((bottomLeft.Y - topLeft.Y) / (double)rows) * i + topLeft.Y);
+                IntPoint leftSideRowBottom = new IntPoint(leftSideRowBottomX, leftSideRowBottomY);
+
+                int rightSideRowBottomX = (int)(((bottomRight.X - topRight.X) / (double)rows) * i + topRight.X);
+                int rightSideRowbottomY = (int)(((bottomRight.Y - topRight.Y) / (double)rows) * i + topRight.Y);
+                IntPoint rightSideRowBottom = new IntPoint(rightSideRowBottomX, rightSideRowbottomY);
+
+                Drawing.Line(imgData, leftSideRowBottom, rightSideRowBottom, colour);
+            }
+
+            //Draw Lines at regular intervals for cols (same as rows, but rotate all of the points around -90 deg)
+            Drawing.Line(imgData, bottomLeft, topLeft, colour);
+            for (int i = 1; i <= cols; i++)
+            {
+                int bottomSideColRightX = (int)(((bottomRight.X - bottomLeft.X) / (double)cols) * i + bottomLeft.X);
+                int bottomSideColRightY = (int)(((bottomRight.Y - bottomLeft.Y) / (double)cols) * i + bottomLeft.Y);
+                IntPoint bottomSideColRight = new IntPoint(bottomSideColRightX, bottomSideColRightY);
+
+                int topSideColRightX = (int)(((topRight.X - topLeft.X) / (double)cols) * i + topLeft.X);
+                int topSideColRightY = (int)(((topRight.Y - topLeft.Y) / (double)cols) * i + topLeft.Y);
+                IntPoint topSideColRight = new IntPoint(topSideColRightX, topSideColRightY);
+
+                Drawing.Line(imgData, bottomSideColRight, topSideColRight, colour);
+            }
+
+            img.UnlockBits(imgData);
+            return img;
+        }
+
+        private IntPoint getTopLeftCoordinate()
+        {
+            int x = int.Parse(txtTopLeftX.Text);
+            int y = int.Parse(txtTopLeftY.Text);
+            return new IntPoint(x, y);
+        }
+
+        private IntPoint getTopRightCoordinate()
+        {
+            int x = int.Parse(txtTopRightX.Text);
+            int y = int.Parse(txtTopRightY.Text);
+            return new IntPoint(x, y);
+        }
+
+        private IntPoint getBottomRightCoordinate()
+        {
+            int x = int.Parse(txtBottomRightX.Text);
+            int y = int.Parse(txtBottomRightY.Text);
+            return new IntPoint(x, y);
+        }
+
+        private IntPoint getBottomLeftCoordinate()
+        {
+            int x = int.Parse(txtBottomLeftX.Text);
+            int y = int.Parse(txtBottomLeftY.Text);
+            return new IntPoint(x, y);
         }
     }
 }
