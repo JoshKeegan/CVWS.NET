123964d5ae69b666688e687ec41422ff362e100f
Josh Keegan <josh.keegan@gmx.com>
2014-03-05 16:32:10 +0000

    Registered Interest system implemented on WordsearchImage

--- ImageMarkup/WordsearchImage.cs
+++ ImageMarkup/WordsearchImage.cs
@@ -11,11 +11,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 using AForge;
+using AForge.Imaging.Filters;
 
 using ImageMarkup.Exceptions;
 using SharedHelpers.Maths;
+using SharedHelpers.Exceptions;
 
 namespace ImageMarkup
 {
@@ -23,6 +26,8 @@ namespace ImageMarkup
     {
         //Private variables
         private string wordsearchId;
+        private Bitmap bitmap;
+        private uint riCountBitmap = 0; //Registered Interest in the Bitmap
 
         //Public Variables
         public IntPoint TopLeft { get; private set; }
@@ -89,6 +94,38 @@ namespace ImageMarkup
             }
         }
 
+        public Bitmap Bitmap
+        {
+            get
+            {
+                //If we don't have the bitmap, make it
+                if(bitmap == null)
+                {
+                    //Check that an interest has been registered in the bitmap (not necessary but should act as an early indicator rather than letting memory leaks happen)
+                    if (riCountBitmap > 0)
+                    {
+                        //Fetch the bitmap of the main image this wordsearch image is in
+                        Image fromImage = FromImage;
+                        fromImage.RegisterInterestInBitmap();
+
+                        //Transform the region containing this wordsearch into a new image
+                        QuadrilateralTransformation quadTransform = new QuadrilateralTransformation(
+                            new List<IntPoint>(Coordinates));
+
+                        bitmap = quadTransform.Apply(fromImage.Bitmap);
+
+                        //Deregister interest in the main Image's Bitmap to allow for it to dispose of it appropriately
+                        fromImage.DeregisterInterestInBitmap();
+                    }
+                    else //Otherwise the caller hasn't registered their interest in the resource before attempting to access it
+                    {
+                        throw new Exception(String.Format("Caller hasn't registered their interest in WordsearchImage of wordsearchID {0}, Image hash {1} before attempting to access it", wordsearchId, FromImageHash));
+                    }
+                }
+                return bitmap;
+            }
+        }
+
         public Dictionary<string, string> MetaData { get; private set; }
 
         //Constructors
@@ -110,6 +147,32 @@ namespace ImageMarkup
             this.MetaData = metaData;
             this.FromImageHash = fromImageHash;
             this.wordsearchId = wordsearchId;
+        }
+
+        //Public methods
+        public void RegisterInterestInBitmap()
+        {
+            riCountBitmap++;
+        }
+
+        public void DeregisterInterestInBitmap()
+        {
+            //If there is currently a registered interest in the bitmap
+            if (riCountBitmap > 0)
+            {
+                riCountBitmap--;
+
+                //If the bitmap is no longer in use (nobody has a registered interest in it), then dispose of it
+                if (riCountBitmap == 0)
+                {
+                    bitmap.Dispose();
+                    bitmap = null;
+                }
+            }
+            else //Otherwise there is no registered in the bitmap object, cannot deregister
+            {
+                throw new RegisteredInterestException(String.Format("Cannot deregister interest in Bitmap for WordsearchImage of wordsearchID {0}, Image hash {1} as there are currently no registered interests", wordsearchId, FromImageHash));
+            }
         }
     }
 }
