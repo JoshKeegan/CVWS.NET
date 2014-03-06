9f6a718f078492d5e651a4c57e3572ac1a749747
Josh Keegan <josh.keegan@gmx.com>
2014-03-06 12:17:49 +0000

    Stubbed CevCharClassification console app

---
+++ DevCharClassification/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Dev Char Classification - Console app written during development of the first character classifier
+ * Program Entry Point
+ * By Josh Keegan 06/03/2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCharClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
