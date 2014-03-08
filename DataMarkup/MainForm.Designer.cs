2f033d1159d20f8995a0eefb2d93f3fd93886865
Josh Keegan <josh.keegan@gmx.com>
2014-03-08 18:59:09 +0000

    MainForm: for some reason this never got committed when it was originally written

---
+++ DataMarkup/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DataEntryGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImageMarkup = new System.Windows.Forms.Button();
+            this.btnWordsearchMarkup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImageMarkup
+            // 
+            this.btnImageMarkup.Location = new System.Drawing.Point(13, 13);
+            this.btnImageMarkup.Name = "btnImageMarkup";
+            this.btnImageMarkup.Size = new System.Drawing.Size(77, 54);
+            this.btnImageMarkup.TabIndex = 0;
+            this.btnImageMarkup.Text = "Image Markup";
+            this.btnImageMarkup.UseVisualStyleBackColor = true;
+            this.btnImageMarkup.Click += new System.EventHandler(this.btnImageMarkup_Click);
+            // 
+            // btnWordsearchMarkup
+            // 
+            this.btnWordsearchMarkup.Location = new System.Drawing.Point(96, 13);
+            this.btnWordsearchMarkup.Name = "btnWordsearchMarkup";
+            this.btnWordsearchMarkup.Size = new System.Drawing.Size(77, 54);
+            this.btnWordsearchMarkup.TabIndex = 1;
+            this.btnWordsearchMarkup.Text = "Word search Markup";
+            this.btnWordsearchMarkup.UseVisualStyleBackColor = true;
+            this.btnWordsearchMarkup.Click += new System.EventHandler(this.btnWordsearchMarkup_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(186, 77);
+            this.Controls.Add(this.btnWordsearchMarkup);
+            this.Controls.Add(this.btnImageMarkup);
+            this.Name = "MainForm";
+            this.Text = "Data Entry";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImageMarkup;
+        private System.Windows.Forms.Button btnWordsearchMarkup;
+    }
+}
---
+++ DataMarkup/MainForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Data Entry GUI
+ * Main Form Class - Main GUI Window
+ * By Josh Keegan 05/03/2013
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEntryGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImageMarkup_Click(object sender, EventArgs e)
+        {
+            //Start the form for marking up images
+            ImageMarkupForm imageMarkupForm = new ImageMarkupForm();
+            this.Hide();
+            imageMarkupForm.ShowDialog();
+            this.Show();
+        }
+
+        private void btnWordsearchMarkup_Click(object sender, EventArgs e)
+        {
+            //TODO: Start the form for marking up wordsearches
+            WordsearchMarkupForm wordsearchMarkupForm = new WordsearchMarkupForm();
+            this.Hide();
+            wordsearchMarkupForm.ShowDialog();
+            this.Show();
+        }
+    }
+}
