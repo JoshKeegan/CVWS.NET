d5dacfe42ed5c4303faeaa31498ec5c27286ae57
Josh Keegan <josh.keegan@gmx.com>
2014-03-08 20:48:35 +0000

    Stubbed Quantitative Evaluation system

---
+++ QuantitativeEvaluation/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Quantitative Evaluation
+ * Program Entry Point
+ * By Josh Keegan 08/03/2013
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantitativeEvaluation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
