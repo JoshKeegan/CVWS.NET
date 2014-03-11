bcef8032ec70fcedcd4687f4d81073714b733c9e
Josh Keegan <josh.keegan@gmx.com>
2014-03-11 18:22:45 +0000

    Allow Feature Extraction algorithms to be trained

---
+++ SharedHelpers/ClassifierInterfacing/FeatureExtraction/TrainableFeatureExtractionAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Shared Helpers
+ * Trainable Feature Extraction Algorithm - abstract class
+ * By Josh Keegan 11/03/2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharedHelpers.Exceptions;
+
+namespace SharedHelpers.ClassifierInterfacing.FeatureExtraction
+{
+    public abstract class TrainableFeatureExtractionAlgorithm : FeatureExtractionAlgorithm
+    {
+        private bool trained = false;
+
+        //Use the public methods here to perform checks about the training before then passing the call on to the child class in the protected DoBlah methods
+        public void Train(Bitmap[] charImgs)
+        {
+            if(trained)
+            {
+                throw new TrainableFeatureExtractionAlgorithmException("Trainable feature extraction algorithm has already been trained");
+            }
+            trained = true;
+
+            DoTrain(charImgs);
+        }
+
+        protected abstract void DoTrain(Bitmap[] charImgs);
+        public override double[][] Extract(Bitmap[] charImgs)
+        {
+            if(!trained)
+            {
+                throw new TrainableFeatureExtractionAlgorithmException("Trainable feature extraction algorithms must be trained before extracting data");
+            }
+
+            return DoExtract(charImgs);
+        }
+
+        protected abstract double[][] DoExtract(Bitmap[] charImgs);
+    }
+}
---
+++ SharedHelpers/Exceptions/UntrainedFeatureExtractionAlgorithmException.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Shared Helpers Exceptions
+ * TrainableFeatureExtractionAlgorithmException
+ * By Josh Keegan 11/03/2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedHelpers.Exceptions
+{
+    public class TrainableFeatureExtractionAlgorithmException : Exception
+    {
+        //Constructors
+        public TrainableFeatureExtractionAlgorithmException() { }
+
+        public TrainableFeatureExtractionAlgorithmException(string message) : base(message) { }
+
+        public TrainableFeatureExtractionAlgorithmException(string message, Exception inner) : base(message, inner) { }
+    }
+}
