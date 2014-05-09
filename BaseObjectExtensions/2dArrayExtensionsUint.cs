044fb863765cfd86ef78acdbbb93c90b51e8775b
Josh Keegan <josh.keegan@gmx.com>
2014-05-09 23:03:10 +0100

    Missing file

---
+++ BaseObjectExtensions/2dArrayExtensionsUint.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Dissertation CV Wordsearch Solver
+ * Base Object Extensions
+ * partial 2D Array Extensions for Uint
+ * By Josh Keegan 03/04/2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseObjectExtensions
+{
+    public static partial class _2dArrayExtensions
+    {
+        public static int[,] ToIntArr(this uint[,] uintArr)
+        {
+            int[,] intArr = new int[uintArr.GetLength(0), uintArr.GetLength(1)];
+
+            for(int i = 0; i < uintArr.GetLength(0); i++)
+            {
+                for(int j = 0; j < uintArr.GetLength(1); j++)
+                {
+                    intArr[i, j] = checked((int)uintArr[i, j]); //throws OverflowException if val > Int32.MaxValue
+                }
+            }
+            return intArr;
+        }
+    }
+}
