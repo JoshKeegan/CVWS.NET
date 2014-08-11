1497331ddb3a7a4cc5174fbb266a118a5cd172ce
Josh Keegan <josh.keegan@gmx.com>
2014-08-11 21:31:37 +0100

    Assembly title & product name updated

--- SharedHelpers/Properties/AssemblyInfo.cs
+++ SharedHelpers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharedHelpers")]
+[assembly: AssemblyTitle("libCVWS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharedHelpers")]
+[assembly: AssemblyProduct("libCVWS")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
